64c1fd54b2a42f3b2ae8d8e8d877270d95e4511e
RazanAl-Quran <quraan_razan@hotmail.com>
2024-03-31 17:03:32 +0300

    feat: Singly LL part 02 has been added

--- Singly-LL/src/App.java
+++ Singly-LL/src/App.java
@@ -9,17 +9,25 @@ public class App {
         x1.insertLast(30);
         x1.insertFirst(15);
         x1.insertLast(40);
+        x1.printLL();
+        // x1.removeFirst();
+        // x1.printLL();
+        // x1.removeLast();
+        // x1.printLL();
+        x1.removeElement(20);
+        x1.printLL();
+
 
         // You are NOT allowed to use the below code
-        LinkedList<String> friendsLinkedList = new LinkedList<String>();
-        friendsLinkedList.add("Jim");
-        friendsLinkedList.add("Jack");
-        friendsLinkedList.addFirst("Jill");
-        friendsLinkedList.addLast("Joe");
-        friendsLinkedList.removeFirst();
-        friendsLinkedList.removeLast();
-        friendsLinkedList.add(1, "Jane");
-        friendsLinkedList.remove(1);
-        System.out.println(friendsLinkedList);
+        // LinkedList<String> friendsLinkedList = new LinkedList<String>();
+        // friendsLinkedList.add("Jim");
+        // friendsLinkedList.add("Jack");
+        // friendsLinkedList.addFirst("Jill");
+        // friendsLinkedList.addLast("Joe");
+        // friendsLinkedList.removeFirst();
+        // friendsLinkedList.removeLast();
+        // friendsLinkedList.add(1, "Jane");
+        // friendsLinkedList.remove(1);
+        // System.out.println(friendsLinkedList);
     }
 }
--- Singly-LL/src/LL.java
+++ Singly-LL/src/LL.java
@@ -15,7 +15,7 @@ public class LL {
 
     public void insertFirst(int v) {
         Node newNode = new Node(v);
-        if(isEmpty()) {
+        if (isEmpty()) {
             head = newNode;
             return;
         }
@@ -25,14 +25,106 @@ public class LL {
 
     public void insertLast(int v) {
         Node newNode = new Node(v);
-        if(isEmpty()) {
+        if (isEmpty()) {
             head = newNode;
             return;
         }
         Node current = head;
-        while(current.next != null) {
+        while (current.next != null) {
             current = current.next;
         }
         current.next = newNode;
     }
+
+    public void printLL() {
+        if (isEmpty()) {
+            System.out.println("LL is empty");
+            return;
+        }
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.value);
+            current = current.next;
+        }
+        System.out.println("***********************");
+
+    }
+
+    public void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("LL is empty");
+            return;
+        }
+        head = head.next;
+
+        // if(head.next == null) {
+        // head = null;
+        // return;
+        // }
+        // if (head.next != null) {
+        // head = head.next;
+        // return;
+        // }
+    }
+
+    public void removeLast() {
+        if (isEmpty()) {
+            System.out.println("LL is empty");
+            return;
+        }
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+        Node current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+
+    }
+
+    public void removeElement(int element) {
+        if (isEmpty()) {
+            System.out.println("LL is empty");
+            return;
+        }
+        if (head.value == element) {
+            head = head.next;
+            return;
+        }
+
+        // Method 1: without using prev
+        // Node current = head;
+
+        // while (current != null && current.next.value != element) {
+        // current = current.next;
+        // }
+        // if (current == null) {
+        // System.out.println("LL doesn't have this value");
+        // return;
+        // }
+        // current.next = current.next.next;
+
+
+        // Method 2: with using prev
+        Node current = head;
+        Node prev = null;
+
+        while (current != null && current.value != element) {
+            prev = current;
+            current = current.next;
+        }
+        if (current == null) {
+            System.out.println("LL doesn't have this value");
+            return;
+        }
+        prev.next = current.next;
+
+    }
+
+    public void mergeLL(LL x2) {
+        // do it by yourself dear
+    }
+
 }
